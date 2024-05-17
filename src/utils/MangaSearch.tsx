51feee85786aa0e50bf07dbfc9344d119ee3ed78
BaoHaiyah <113325458+BaoHaiyah@users.noreply.github.com>
2024-05-17 21:52:24 +0700

    UI for manga details page (almost complete) + search

--- src/utils/MangaSearch.tsx
+++ src/utils/MangaSearch.tsx
@@ -2,58 +2,59 @@ import axios from 'axios';
 
 const searchManga = async (
     title: string,
-    limit: any = 100,
-    offset: any = 0,
-    authorOrArtist: any = '' /* uuid */,
-    authors: any[] = [],
-    artists: any[] = [],
-    year: any = 0,
-    availableTranslatedLanguage: any = [] /* 'en', 'vi', 'ja' */,
-    originalLanguage: any = [],
-    publicationDemographic: any[] = [],
-    ids: any = [] /* max 100 ids */,
-    contentRating: any = [],
-    excludedTagNames: any = [],
+    limit: number = 20,
+    offset: number = 0,
+    authorOrArtist: string = '',
+    authors: string[] = [],
+    artists: string[] = [],
+    year: number = 0,
+    availableTranslatedLanguage: string[] = [],
+    originalLanguage: string[] = [],
+    publicationDemographic: string[] = [],
+    ids: string[] = [],
+    contentRating: string[] = [],
+    excludedTagNames: string[] = [],
     excludedTagsMode: string = 'AND',
-    includedTagNames: any = [],
+    includedTagNames: string[] = [],
     includedTagsMode: string = 'AND',
-    status: any,
-    order: any = [],
-    filters: any = [],
-    createdAtSince: any = '',
-    updatedAtSince: any = '',
+    status: string = '',
+    order: any = {},
+    filters: any[] = [],
+    createdAtSince: string = '',
+    updatedAtSince: string = '',
 ) => {
-
     const baseUrl = 'https://api.mangadex.org';
 
     try {
-        const resp = await axios.get(`${baseUrl}/manga`, {
-            params: {
-                title: title,
-                limit: limit,
-                offset: offset,
-                authorOrArtist: authorOrArtist,
-                authors: authors.length === 0 ? null : authors,
-                artists: artists.length === 0 ? null : artists,
-                year: year >= 0 ? year : null,
-                availableTranslatedLanguage: availableTranslatedLanguage,
-                originalLanguage: originalLanguage,
-                publicationDemographic: publicationDemographic,
-                ids: ids /* max 100 ids */,
-                contentRating: contentRating,
-                excludedTagNames: excludedTagNames,
-                excludedTagsMode: excludedTagsMode,
-                includedTagNames: includedTagNames,
-                includedTagsMode: includedTagsMode,
-                status: status,
-                order: order,
-                filters: filters = [],
-                createdAtSince: createdAtSince,
-                updatedAtSince: updatedAtSince,
-            }
-        });
+        const params: any = {
+            title: title,
+            limit: limit,
+            offset: offset,
+        };
 
-        return resp.data.data.map((manga: any) => manga.id);
+        if (authorOrArtist) params.authorOrArtist = authorOrArtist;
+        if (authors.length > 0) params.authors = authors;
+        if (artists.length > 0) params.artists = artists;
+        if (year > 0) params.year = year;
+        if (availableTranslatedLanguage.length > 0) params.availableTranslatedLanguage = availableTranslatedLanguage;
+        if (originalLanguage.length > 0) params.originalLanguage = originalLanguage;
+        if (publicationDemographic.length > 0) params.publicationDemographic = publicationDemographic;
+        if (ids.length > 0) params.ids = ids;
+        if (contentRating.length > 0) params.contentRating = contentRating;
+        if (excludedTagNames.length > 0) params.excludedTagNames = excludedTagNames;
+        if (excludedTagsMode) params.excludedTagsMode = excludedTagsMode;
+        if (includedTagNames.length > 0) params.includedTagNames = includedTagNames;
+        if (includedTagsMode) params.includedTagsMode = includedTagsMode;
+        if (status) params.status = status;
+        if (Object.keys(order).length > 0) params.order = order;
+        if (filters.length > 0) params.filters = filters;
+        if (createdAtSince) params.createdAtSince = createdAtSince;
+        if (updatedAtSince) params.updatedAtSince = updatedAtSince;
+
+        const resp = await axios.get(`${baseUrl}/manga?includes[]=cover_art&includes[]=author&includes[]=artist`, { params });
+
+        console.log(resp.data)
+        return resp.data;
 
     } catch (error) {
         console.error("Error searching manga:", error);
