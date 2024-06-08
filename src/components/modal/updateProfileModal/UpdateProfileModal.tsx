b69ad52c968f32f0a1f77005c183848548927f3a
TamTLeader <21521408@gm.uit.edu.vn>
2024-06-08 11:09:13 +0700

    update profile update modal

--- src/components/modal/updateProfileModal/UpdateProfileModal.tsx
+++ src/components/modal/updateProfileModal/UpdateProfileModal.tsx
@@ -1,24 +1,99 @@
-import { Avatar, Button, IconButton, Modal, TextField, Typography } from '@mui/material'
-import './updateProfileModal.scss'
-import { useRef } from 'react';
+import { Avatar, Button, IconButton, Modal, TextField, Typography } from '@mui/material';
+import './updateProfileModal.scss';
+import { useRef, useState, useEffect } from 'react';
+
 interface UPModalProps {
     open: boolean;
     handleClose: () => void;
-    userid?: string;
+    uid: string;
+    background: string; // link, có thể rỗng vì user không có sẵn hình
+    avatar: string; // link, có thể rỗng vì user không có sẵn hình
+    name: string;
+    bio: string;
+    link: string;
 }
+
 const UpdateProfileModal = (props: UPModalProps) => {
-    const fileInputRef = useRef<HTMLInputElement>(null);
-    const handleImageChange = () => {
-        console.log("none");
-    }
-    const handleSend = () => {
-        console.log("Content 1:");
-        console.log("Content 2:");
-        //liệt kê 2 tấm hình ở đây bằng console log
-        //cho tất cả bằng close
-        props.handleClose();
+    const [name, setName] = useState<string>(props.name || '');
+    const [bio, setBio] = useState<string>(props.bio);
+    const [link, setLink] = useState<string>(props.link);
+    const [background, setBackground] = useState<string>(props.background);
+    const [avatar, setAvatar] = useState<string>(props.avatar);
+    const [backgroundFile, setBackgroundFile] = useState<File | null>(null);
+    const [avatarFile, setAvatarFile] = useState<File | null>(null);
+    const [isSendButtonDisabled, setIsSendButtonDisabled] = useState<boolean>(true);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [nameError, setNameError] = useState<string | null>(null);
+    const backgroundInputRef = useRef<HTMLInputElement>(null);
+    const avatarInputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (
+            name !== props.name ||
+            bio !== props.bio ||
+            link !== props.link ||
+            backgroundFile !== null ||
+            avatarFile !== null
+        ) {
+            setIsSendButtonDisabled(false);
+        } else {
+            setIsSendButtonDisabled(true);
+        }
+    }, [name, bio, link, backgroundFile, avatarFile]);
+
+    const handleBackgroundChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setBackgroundFile(event.target.files[0]);
+            setBackground(URL.createObjectURL(event.target.files[0]));
+        }
     };
-    const isSendButtonDisabled = true;//sửa logic nếu không có gì thay đổi thì disable
+
+    const handleAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setAvatarFile(event.target.files[0]);
+            setAvatar(URL.createObjectURL(event.target.files[0]));
+        }
+    };
+
+    const handleSend = async () => {
+        if (name.trim() === '') {
+            setNameError('Name must not be empty');
+            return;
+        }
+
+        try {
+            // Simulate an API call
+           
+            console.log("Name:", name);
+            console.log("Bio:", bio);
+            console.log("Link:", link);
+            console.log("Background Image File:", backgroundFile);
+            console.log("Avatar Image File:", avatarFile);
+
+            // Clear all inputs
+            setName(props.name);
+            setBio(props.bio);
+            setLink(props.link);
+            setBackground(props.background);
+            setAvatar(props.avatar);
+            setBackgroundFile(null);
+            setAvatarFile(null);
+
+            props.handleClose();
+            setTimeout(() => {
+            window.location.reload();
+            },5000);
+        } catch (error) {
+            // Handle error
+            setErrorMessage('Update failed, please try again in 10 seconds');
+            setIsSendButtonDisabled(true);
+            setTimeout(() => {
+                setErrorMessage(null);
+                setIsSendButtonDisabled(false);
+            }, 10000);
+        }
+    };
+
     return (
         <Modal
             open={props.open}
@@ -36,69 +111,88 @@ const UpdateProfileModal = (props: UPModalProps) => {
                         X
                     </IconButton>
                 </div>
+                {errorMessage && (
+                    <Typography className="error-message" variant="body2" component="p">
+                        {errorMessage}
+                    </Typography>
+                )}
                 <div className="imageInput">
-                    <div className="background">
-                        <img src="" alt="" />
+                    <div
+                        className="background"
+                        onClick={() => backgroundInputRef.current?.click()}
+                    >
+                        {background && (
+                            <img src={background} alt="Background" />
+                        )}
                         <input
                             type="file"
                             accept="image/*"
-                            ref={fileInputRef}
-                            onChange={handleImageChange}
+                            ref={backgroundInputRef}
+                            onChange={handleBackgroundChange}
                             hidden
-                            multiple
                         />
                     </div>
-                    <div className="avatar">
+                    <div
+                        className="avatar"
+                        onClick={() => avatarInputRef.current?.click()}
+                    >
                         <Avatar
                             className="Avatar"
-                            src="https://cdn.donmai.us/original/5f/ea/__firefly_honkai_and_1_more_drawn_by_baba_ba_ba_mb__5feaaa99527187a3db0e437380ec3932.jpg"
+                            src={avatar}
                             alt="avatar"
-                            sx={{ width: "128px", height: "128px", border: "4px solid #1F1F1F" }} />
+                            sx={{ width: "128px", height: "128px", border: "4px solid #1F1F1F" }}
+                        />
                         <input
                             type="file"
                             accept="image/*"
-                            ref={fileInputRef}
-                            onChange={handleImageChange}
+                            ref={avatarInputRef}
+                            onChange={handleAvatarChange}
                             hidden
-                            multiple
                         />
                     </div>
                 </div>
                 <div className="textInput">
                     <div className="name">
                         <TextField
-                            placeholder="Enter comicId here (copy series code from link)"
+                            label="Your display name"
                             variant="outlined"
                             fullWidth
                             rows={1}
-                            value={mangaSuggestContent}
-                            onChange={(e) => setMangaSuggestContent(e.target.value)}
-                            className="mangaSuggestField"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                                if (e.target.value.trim() !== '') {
+                                    setNameError(null);
+                                }
+                            }}
+                            className="nameInputField"
+                            error={!!nameError}
+                            helperText={nameError}
                         />
                     </div>
                     <div className="bio">
                         <TextField
-                            placeholder="What's on your mind?"
+                            label="Bio"
                             variant="outlined"
                             fullWidth
                             multiline
                             minRows={3}
                             maxRows={10}
-                            value={postContent}
-                            onChange={(e) => setPostContent(e.target.value)}
+                            value={bio}
+                            onChange={(e) => setBio(e.target.value)}
                             sx={{ mt: 2, mb: 2 }}
                             className='customScrollbar contentField'
                         />
                     </div>
                     <div className="link">
                         <TextField
-                            placeholder="Enter comicId here (copy series code from link)"
+                            label="Website"
                             variant="outlined"
                             fullWidth
                             rows={1}
-                            value={mangaSuggestContent}
-                            onChange={(e) => setMangaSuggestContent(e.target.value)}
-                            className="mangaSuggestField"
+                            value={link}
+                            onChange={(e) => setLink(e.target.value)}
+                            className="linkField"
                         />
                     </div>
                 </div>
@@ -114,7 +208,7 @@ const UpdateProfileModal = (props: UPModalProps) => {
                 </div>
             </div>
         </Modal>
-    )
-}
+    );
+};
 
-export default UpdateProfileModal
+export default UpdateProfileModal;
