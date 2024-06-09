bb7fbf0957c2fb55a9c8cb5e93a5a635667c7628
BaoHaiyah <113325458+BaoHaiyah@users.noreply.github.com>
2024-06-09 22:05:12 +0700

    adjust in banner

--- src/pages/banner/Banner.tsx
+++ src/pages/banner/Banner.tsx
@@ -3,6 +3,7 @@ import './Banner.scss';
 
 import Image from '../image/Image';
 import Tag from '../tag/Tag';
+import { NavLink } from 'react-router-dom';
 
 interface BannerProps {
     rank: any
@@ -14,7 +15,7 @@ const Banner: React.FC<BannerProps> = ({ manga, rank }) => {
     const [author, setAuthor] = useState('');
     const [artist, setArtist] = useState('');
 
-    const getCover = async() => {
+    const getCover = async () => {
         // manga.relationships.forEach((relate: any) => {
         //     if (relate.type === 'cover_art') {
         //         setCover(relate.attributes?.fileName);
@@ -27,34 +28,40 @@ const Banner: React.FC<BannerProps> = ({ manga, rank }) => {
 
     useEffect(() => {
         getCover();
-      }, []);
+    }, []);
 
-  return (
-    <div className="banner-container">
-        {/* <div className="background-img" style={{backgroundImage: `url(https://uploads.mangadex.org/covers/${manga.id}/${cover}.512.jpg)`}}/> */}
-        <div className="background-img">
-          <img src={'https://uploads.mangadex.org/covers/' + manga.id + '/' + cover + '.512.jpg'} alt={manga.attributes.title.en}/>
-        </div>
-        <div className="banner-content">
-            <div className="banner-image">
-                <Image src={'https://uploads.mangadex.org/covers/' + manga.id + '/' + cover + '.512.jpg'} alt={manga.attributes.title.en} ratio="4/6" />
-            </div>
-            <div className="banner-info">
-                <div>
-                    <h2>{((manga.attributes.title.en) || (manga.attributes.title.ja)) + ' - NO.' + rank}</h2>
-                    <div className="banner-tags">
-                        {manga.attributes.tags.map((tag: any) => (
-                            <Tag key={tag.id} tag={tag}/>
-                        ))}
-                    </div>
-                    <p>{manga.attributes.description.en}</p>
+    return (
+        <div className="banner-container">
+            {/* <div className="background-img" style={{backgroundImage: `url(https://uploads.mangadex.org/covers/${manga.id}/${cover}.512.jpg)`}}/> */}
+            <NavLink to={`/manga/${manga.id}`} style={{ textDecoration: 'none' }}>
+                <div className="background-img">
+                    <img src={'https://uploads.mangadex.org/covers/' + manga.id + '/' + cover + '.512.jpg'} alt={manga.attributes.title.en} />
                 </div>
-                <h5>{(author === artist) ? (author) : (author + ', ' + artist)}</h5>
-                {/* Add more info sections here */}
+            </NavLink>
+            <div className="banner-content">
+                <NavLink to={`/manga/${manga.id}`} style={{ textDecoration: 'none' }}>
+                    <div className="banner-image">
+                        <Image src={'https://uploads.mangadex.org/covers/' + manga.id + '/' + cover + '.512.jpg'} alt={manga.attributes.title.en} ratio="4/6" />
+                    </div>
+                </NavLink>
+                <div className="banner-info">
+                    <div>
+                        <NavLink to={`/manga/${manga.id}`} style={{ textDecoration: 'none' }}>
+                            <h2>{((manga.attributes.title.en) || (manga.attributes.title.ja)) + ' - NO.' + rank}</h2>
+                        </NavLink>
+                        <div className="banner-tags">
+                            {manga.attributes.tags.map((tag: any) => (
+                                <Tag key={tag.id} tag={tag} />
+                            ))}
+                        </div>
+                        <p>{manga.attributes.description.en}</p>
+                    </div>
+                    <h5>{(author === artist) ? (author) : (author + ', ' + artist)}</h5>
+                    {/* Add more info sections here */}
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Banner;
